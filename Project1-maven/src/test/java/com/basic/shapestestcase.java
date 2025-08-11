5cc65e5e8154f3dc41daae577c4620384451123d
Kancham mahitha <Kancham mahitha@LAPTOP-V4F9A9DH>
2025-08-12 02:10:39 +0530

    Minimize code with inline statements, declare Shape as instance variable, update passed/failed test cases

--- Project1-maven/src/test/java/com/basic/shapestestcase.java
+++ Project1-maven/src/test/java/com/basic/shapestestcase.java
@@ -5,22 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class shapestestcase {
+	shapes Shape=new shapes();
 
 	@Test
 	void testSqaure() {
-		shapes Shape=new shapes();
-		double actual=Shape.computeSquareArea(5);
-		double expected= 25;
-		assertEquals(expected,actual);
+		assertEquals((double) 25,Shape.computeSquareArea(5));
 		
 	}
 	@Test
 	void testCircle() {
-		shapes Shape=new shapes();
-		double actual=Shape.computeCircleArea(5);
-		double expected= 78.5;
-		assertEquals(expected,actual);
-		
+		assertEquals(78.5,Shape.computeCircleArea(5));
+		assertEquals(78.5,Shape.computeCircleArea(-1),"Testcase failed");
 	}
 
 }
