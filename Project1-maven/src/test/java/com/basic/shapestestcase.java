84e8c8975c7468bcaa108d20a9bdd41165922f21
Kancham mahitha <Kancham mahitha@LAPTOP-V4F9A9DH>
2025-08-12 02:00:35 +0530

    Added two testcases for shapes logic and test cases are passed

--- Project1-maven/src/test/java/com/basic/shapestestcase.java
+++ Project1-maven/src/test/java/com/basic/shapestestcase.java
@@ -7,8 +7,20 @@ import org.junit.jupiter.api.Test;
 class shapestestcase {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSqaure() {
+		shapes Shape=new shapes();
+		double actual=Shape.computeSquareArea(5);
+		double expected= 25;
+		assertEquals(expected,actual);
+		
+	}
+	@Test
+	void testCircle() {
+		shapes Shape=new shapes();
+		double actual=Shape.computeCircleArea(5);
+		double expected= 78.5;
+		assertEquals(expected,actual);
+		
 	}
 
 }
