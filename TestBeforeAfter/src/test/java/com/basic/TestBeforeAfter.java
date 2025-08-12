f6ad3e553f6ec27292ca91f358fa8b90b56d53db
Kancham mahitha <Kancham mahitha@LAPTOP-V4F9A9DH>
2025-08-12 18:43:40 +0530

    Added testcases using @BeforeEach

---
+++ TestBeforeAfter/src/test/java/com/basic/TestBeforeAfter.java
@@ -0,0 +1,31 @@
+package com.basic;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestBeforeAfter 
+{
+	Shapes shape;
+	
+	@BeforeEach
+	void init()
+	{
+		shape=new Shapes();
+		System.out.println("Before test");
+	}
+
+	@Test
+	void testcomputeSquareArea() 
+	{
+		assertEquals(25,shape.computeSquareArea(5));
+		System.out.println("Actual test running");
+	}
+	@Test
+	void testcomputeCircleArea() 
+	{
+		assertEquals(78.5,shape.computeCircleArea(5));
+		System.out.println("Before test running");
+	}
+
+}
