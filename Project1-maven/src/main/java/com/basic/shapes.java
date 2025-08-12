7b53b2b4b527ed874a4490b36b31f657820a5f4c
Kancham mahitha <Kancham mahitha@LAPTOP-V4F9A9DH>
2025-08-12 12:59:14 +0530

    updated logic

--- Project1-maven/src/main/java/com/basic/shapes.java
+++ Project1-maven/src/main/java/com/basic/shapes.java
@@ -1,5 +1,17 @@
 package com.basic;
 
-public class shapes {
+public class shapes 
+{
+	public double computeSquareArea(double length)
+	{
+		double area =length * length;
+		return area;
+	}
+	
+	public double computeCircleArea(double radius)
+	{
+		double result =3.14*radius * radius;
+		return result;
+	}
 
 }
