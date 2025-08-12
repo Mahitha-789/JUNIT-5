2b712c267d409f19f76c6524dbfddbdc5d05370d
Kancham mahitha <Kancham mahitha@LAPTOP-V4F9A9DH>
2025-08-12 15:26:51 +0530

    added fail condition if there is no exception test will fail

--- ArrayExeption/src/test/java/com/basic/Testexception.java
+++ ArrayExeption/src/test/java/com/basic/Testexception.java
@@ -1,6 +1,8 @@
 package com.basic;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
 
 class Testexception {
 
@@ -9,13 +11,14 @@ class Testexception {
 	{
 		try {
 		Sortingarray array=new Sortingarray();
-		int unsorted[]= null;
+		int unsorted[]= {2,8,6} ;
 		int sortedArray[]=array.sortingArray(unsorted);
 		for(int elem:sortedArray)
 		{
 			System.out.print(elem);
-				}
+		}
 		System.out.println("Statements below exeception");
+		fail();
 		}
 		catch(NullPointerException e)
 		{
