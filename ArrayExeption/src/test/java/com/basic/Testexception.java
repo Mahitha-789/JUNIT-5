375371d7b2d3e29e91710eedeba6c570b43c1e69
Kancham mahitha <Kancham mahitha@LAPTOP-V4F9A9DH>
2025-08-12 15:36:43 +0530

    Using assertThrows for exception

--- ArrayExeption/src/test/java/com/basic/Testexception.java
+++ ArrayExeption/src/test/java/com/basic/Testexception.java
@@ -1,31 +1,31 @@
 package com.basic;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
-
+//import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class Testexception {
 
 	@Test
 	void test() 
 	{
-		try {
+//		try {
 		Sortingarray array=new Sortingarray();
-		int unsorted[]= {2,8,6} ;
-		int sortedArray[]=array.sortingArray(unsorted);
-		for(int elem:sortedArray)
-		{
-			System.out.print(elem);
-		}
-		System.out.println("Statements below exeception");
-		fail();
-		}
-		catch(NullPointerException e)
-		{
-			System.out.println("Exception genarated");
-			
-		}
-		
+//		int unsorted[]= {2,8,6} ;
+//		int sortedArray[]=array.sortingArray(unsorted);
+//		for(int elem:sortedArray)
+//		{
+//			System.out.print(elem);
+//		}
+//		System.out.println("Statements below exeception");
+//		fail();
+//		}
+//		catch(NullPointerException e)
+//		{
+//			System.out.println("Exception genarated");			
+//		}
+		int unsorted[]= null;
+		assertThrows(NullPointerException.class,()-> array.sortingArray(unsorted));
 		
 		
 	}
